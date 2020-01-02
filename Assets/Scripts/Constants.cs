e9aa78b27395b5ffdf6a9124b844c8fd4c55ea04
FreezingFyre <freezingfyre@yahoo.com>
2020-01-02 16:27:24 -0500

    Fixed the collision/movement system

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants {
+
+    public enum Color {
+        BLUE,
+        BROWN,
+        DARK_BLUE,
+        GREEN,
+        ORANGE,
+        PURPLE,
+        RED,
+        YELLOW
+    };
+
+    public const int width = 21;
+    public const int height = 13;
+
+    public const float autogenWallRate = 0.15f;
+
+    public const float playerSpeed = 4.0f;
+
+}
