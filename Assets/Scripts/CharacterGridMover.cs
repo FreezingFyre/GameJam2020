8333023bc23690de02c05fa376788f27dc4bd892
FreezingFyre <freezingfyre@yahoo.com>
2020-01-02 00:35:17 -0500

    Finished the freakin movement

---
+++ Assets/Scripts/CharacterGridMover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterGridMover : GridMover
+{
+
+    private Direction axisDirection;
+    private bool canMove;
+
+    // Start is called before the first frame update
+    public override void ChildStart() {
+        axisDirection = Direction.None;
+        canMove = true;
+    }
+
+    // When the object reaches the cursor
+    public override void ReachedCursorAction() {
+        if (axisDirection != Direction.None) {
+            canMove = !MoveCursor(directions[axisDirection]);
+        } else {
+            canMove = true;
+        }
+    }
+
+    // OnMove sets the internal notion of which way the joystick is pointing
+    void OnMove(InputValue input) {
+        Vector2 axes = input.Get<Vector2>();
+        if (axes.x != 0.0f || axes.y != 0.0f) {
+            if (Mathf.Abs(axes.x) > Mathf.Abs(axes.y)) {
+                axisDirection = (axes.x > 0) ? Direction.Right : Direction.Left;
+            } else {
+                axisDirection = (axes.y > 0) ? Direction.Up : Direction.Down;
+            }
+        } else {
+            axisDirection = Direction.None;
+        }
+        if (canMove && axisDirection != Direction.None) {
+            canMove = !MoveCursor(directions[axisDirection]);
+        }
+    }
+
+}
