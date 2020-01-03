36d38248ad307ec89dee8596a071a4e8c5588ecf
bnackoud <bnackoud@gmail.com>
2020-01-02 23:41:52 -0500

    added much more functionality to multiplayer support

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -5,13 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerMover : GridMover {
 
+    private int playerNumber;
+    private Constants.Color color;
     private Vector2Int axisDirection;
     private bool canMove;
+    private GameObject paint;
+    private GameObject paintBomb;
 
     // Start is called before the first frame update
     public override void ChildStart() {
         canMove = true;
         moveSpeed = Constants.playerSpeed;
+        paint = Resources.Load("Prefabs/Paint") as GameObject;
+        paintBomb = Resources.Load("Prefabs/PaintBomb") as GameObject;
+
+        var paintColor = Constants.paintColors[MapController.playerCount];
+        GetComponent<SpriteRenderer>().color = paintColor;
+        this.color = Constants.ColorToEnum[paintColor];
+
+        MapController.playerCount++;
+        playerNumber = MapController.playerCount;
     }
 
     // When the object reaches the cursor
@@ -35,7 +48,7 @@ public class PlayerMover : GridMover {
     }
 
     // OnMove sets the internal notion of which way the joystick is pointing
-    void OnMove(InputValue input) {
+    void OnMainPMove(InputValue input) {
         Vector2 axes = input.Get<Vector2>();
         if (axes.x != 0.0f || axes.y != 0.0f) {
             if (Mathf.Abs(axes.x) > Mathf.Abs(axes.y)) {
@@ -50,6 +63,17 @@ public class PlayerMover : GridMover {
             canMove = false;
             MoveCursor(axisDirection);
         }
+    }
+
+    void OnMainPSquirt(InputValue input)
+    {
+
+    }
+
+    void OnMainPBomb(InputValue input)
+    {
+        PaintBomb bomb = Instantiate(paintBomb, transform.position, Quaternion.identity).GetComponent<PaintBomb>();
+        bomb.DelayStart(paint, 2, .05f, 4);
     }
 
 }
