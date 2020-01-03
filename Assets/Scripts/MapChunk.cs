49cb69a2feb5b07e3ad3e8fa6769fe621a7bbd4f
Michael LaBarbera <mlabarbera@oakland.edu>
2020-01-03 13:47:15 -0500

    Play button

---
+++ Assets/Scripts/MapChunk.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChunk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public string[] bitStrong = new[] { "00000", "00000", "00000", "00000", "00000" };
+
+    
+
+    void Start()
+    {
+        
+    }
+}
